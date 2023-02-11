2b52951931f6464767b32714614d3b1172fadee7
Funshoalice <oluwafunshoalice@yahoo.com>
2023-02-11 07:08:35 -0800

    Task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""The script takes the URL and an email address from command line
-arguments, encodes the data to be sent in the request, 
-creates a request object with the data, opens the URL, reads the response, 
-decodes it, and prints the response.
 """
-
+Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response
+"""
+from urllib import request, parse
 import sys
-import urllib.request
-import urllib.parse
+
 
 if __name__ == "__main__":
-          url = sys.argv[1] #Getting the URL from command line arguments
-          value = {"email": sys.argv[2]} #Creating a dictionary with email value
-
-          # Encoding the data to be sent in the request
-          data = urllib.parse.urlencode(value).encode("ascii")
-
-          # Creating request object with data to be sent
-          request = urllib.request.Request(url, data)
-
-          # Opening the url and reading the response
-          with urllib.request.urlopen(request) as response:
-              # Decoding the response and printing it
-              print(response.read().decode("utf-8"))
+    values = {'email': sys.argv[2]}
+    data = parse.urlencode(values)
+    data = data.encode('ascii')
+    req = request.Request(sys.argv[1], data)
+    with request.urlopen(req) as response:
+        body = response.read()
+        print(body.decode('utf-8'))
