2da1f42c28431e220ece14b59dc4800366cf79ac
Funshoalice <oluwafunshoalice@yahoo.com>
2023-02-11 07:28:19 -0800

    Task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-"""The script takes the URL and an email address 
-from command line arguments, creates
-a dictionary with email value, makes a POST 
-request to the URL with the data, and prints the response content
+"""A  script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response.
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1] #Getting the URL from command line arguments
-    value = {"email": sys.argv[2]} #Creating a dictionary with email value
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
 
-    #Making a POST request to the url with data
     r = requests.post(url, data=value)
-
-    #Printing the response content
     print(r.text)
