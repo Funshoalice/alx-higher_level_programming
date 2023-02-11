4958c19a99937e4fb62837e3e04159e3c286c50b
Funshoalice <oluwafunshoalice@yahoo.com>
2023-02-11 07:25:21 -0800

    Task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,24 +1,18 @@
-!/usr/bin/python3
-"""The script takes one command line argument, the URL,
-and makes a GET request to that URL using the requests library. 
-It then checks the status code of the response and prints the 
-error code if the status code is greater than or equal to 400. 
-If the status code is less than 400, it prints the response content
+#!/usr/bin/python3
+"""A script that
+- takes in a URL
+- sends a request to the URL
+- displays the body of the response.
 """
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1] #Getting the URL from command line arguments
+    url = sys.argv[1]
 
-    #Making a GET request to the url
     r = requests.get(url)
-
-    #Checking the status code of the response
     if r.status_code >= 400:
-        #Printing the error code if the status code is greater than or equal to 400
         print("Error code: {}".format(r.status_code))
     else:
-        #Printing the response content if the status code is less than 400
         print(r.text)
