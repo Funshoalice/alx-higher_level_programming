f7af23b0ac2c1ca26a0534df0791dbba239a8887
Funshoalice <oluwafunshoalice@yahoo.com>
2023-02-11 07:33:35 -0800

    Task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""The script takes the URL from command line arguments, makes a GET request 
-to that URL using the requests library, then it extracts the value of the 
-X-Request-Id header and prints it
+"""Displays the X-Request-Id header variable of a request to a given URL
 """
 import sys
 import requests
