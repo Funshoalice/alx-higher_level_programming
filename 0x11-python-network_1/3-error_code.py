d540862c46cd69ce540de7de648fb0d6a64e060a
Funshoalice <oluwafunshoalice@yahoo.com>
2023-02-11 07:10:57 -0800

    Task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python3
-"""creates a request object, opens the URL and reads the response, 
-decodes and prints it. In case of a HTTPError, 
-it handles it by printing the error code.
 """
+Python script that takes in a URL, sends a request and displays
+the value of the X-Request-Id variable found in the header
+(handling HTTP errors)
+"""
+from urllib import request, error
 import sys
-import urllib.error
-import urllib.request
+
 
 if __name__ == "__main__":
-      url = sys.argv[1] #Getting the URL from command line arguments
-
-      # Creating request object
-      request = urllib.request.Request(url)
-
-      try:
-          # Opening the url and reading the response
-          with urllib.request.urlopen(request) as response:
-              # Decoding the response and printing it
-              print(response.read().decode("ascii"))
-      except urllib.error.HTTPError as e:
-          # Handling the HTTPError and printing the error code
-          print("Error code: {}".format(e.code))
+    try:
+        with request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except error.HTTPError as err:
+        print('Error code: {}'.format(err.code))
